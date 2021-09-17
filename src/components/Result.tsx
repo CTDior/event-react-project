7ca128fa2abec478146e1cb71b8fd5e60e088ba2
matthew6 <m.holland793@gmail.com>
2021-09-17 17:01:16 -0400

    added details to result including pictures

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -7,11 +7,19 @@ interface Props {
 
 const Result = ({ event }: Props) => {
   let imagesArray = event.images[0].url;
-
+  let venueName = event._embedded.venues[0].name;
+  // Write a function here to convert military time to 
+  // regular time.
   return (
     <li className="Result">
       <p> {event.name} </p>
       <img src={imagesArray} alt={event.name} />
+      <p>{event.dates.start.localDate}</p>
+      <p>{event.dates.start.localTime}</p>
+      <p>{venueName}</p>
+      <a href={event.url}>Ticketmaster Link</a>
+
+
     </li>
   );
 };
--- src/models/Event.ts
+++ src/models/Event.ts
@@ -1,26 +1,31 @@
+interface Venue {
+  name: string;
+  // name inside "venues[]" inside "_embedded{}" inside "events{}"
+}
+
+interface VenuesArray {
+  venues: Venue[];
+}
+
+
 interface Image {
   url: string;
 }
 
 interface Start {
-  date: string;
-  time: string;
+  start: DateAndTime
+}
+
+interface DateAndTime {
+  localDate: string;
+  localTime: string;
   // pull from localDate and localTime in "start{}" inside of "dates{}"
-}
-
-interface Dates {
-  dates: Start;
-}
-
-interface Venue {
-  name: string;
-  // name inside "venues[]" inside "_embedded{}" inside "events{}"
 }
 
 export default interface Event {
   name: string;
   url: string;
   images: Image[];
-  dates: Dates;
-  venue: Venue;
+  dates: Start;
+  _embedded: VenuesArray;
 }
