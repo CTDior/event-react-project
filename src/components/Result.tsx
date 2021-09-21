bb94a1e9a333059a044560e40f67933e6c717344
Emilie Tole <eetole@gmail.com>
2021-09-21 11:58:18 -0400

    styled resultList and result

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -10,20 +10,18 @@ interface Props {
 const Result = ({ event }: Props) => {
   let imagesArray = event.images[0].url;
   let venueName = event._embedded.venues[0].name;
-  // Write a function here to convert military time to 
+  // Write a function here to convert military time to
   // regular time.
-  let day = ""
-  
-  const {favorites, addFavorite, removeFavorite} = useContext(FavoritesContext); // this gets the add favorite function from context
- 
+  let day = "";
+
+  const { favorites, addFavorite, removeFavorite } =
+    useContext(FavoritesContext); // this gets the add favorite function from context
+
   const isFav = (url: string): boolean => {
-    return favorites.some( (event) => {
+    return favorites.some((event) => {
       return event.url === url;
-    })
-  }
-
-
-
+    });
+  };
 
   return (
     <li className="Result">
@@ -32,16 +30,24 @@ const Result = ({ event }: Props) => {
       <p>{event.dates.start.localDate}</p>
       <p>{event.dates.start.localTime}</p>
       <p>{venueName}</p>
-      <a href={event.url}>Ticketmaster Link</a>
+      <a className="tmlink" href={event.url}>
+        Get Your Tickets Here!
+      </a>
       <p>
-      {isFav(event.url) === false &&
-        <button className="favorite" onClick={()=>addFavorite(event)} >Add to Bucket List &hearts;</button>
-        }
-      {isFav(event.url) &&
-        <button className="favorite" onClick={()=>removeFavorite(event.url)}>Remove from Bucket List</button>
-      }
+        {isFav(event.url) === false && (
+          <button className="favorite" onClick={() => addFavorite(event)}>
+            Add to Bucket List &hearts;
+          </button>
+        )}
+        {isFav(event.url) && (
+          <button
+            className="favorite"
+            onClick={() => removeFavorite(event.url)}
+          >
+            Remove from Bucket List
+          </button>
+        )}
       </p>
-        
     </li>
   );
 };
